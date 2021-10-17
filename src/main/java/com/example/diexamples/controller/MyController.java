1d45b81e3757c68f16e9268012de9c1e908cc312
paylinesales <jone.kirkpatrick@gmail.com>
2021-10-17 00:39:52 -0700

    PrimaryBean example, made new service PrimaryGrettingService and set it as primary - then when you call a greeting service without using a qualifier, this new primary bean is used

--- src/main/java/com/example/diexamples/controller/MyController.java
+++ src/main/java/com/example/diexamples/controller/MyController.java
@@ -1,11 +1,17 @@
 package com.example.diexamples.controller;
 
+import com.example.diexamples.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("hello there");
-        return "HELLO";
+        return greetingService.sayGreeting();
     }
 }
